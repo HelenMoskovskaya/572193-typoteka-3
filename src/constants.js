4260927930cb0c9ae184b8efa58db782cb0ab56d
Helen Moskovskaya <helenmoskovskaya@gmail.com>
2021-04-05 16:45:15 +0300

    Хрустящий CRUD path 1

--- src/constants.js
+++ src/constants.js
@@ -6,6 +6,7 @@ const USER_ARGV_INDEX = 2;
 const MAX_ANNOUNCE_COUNT = 5;
 const FILE_MOCKS_NAME = `mocks.json`;
 const MAX_PUBLICATION_COUNT = 1000;
+const MAX_COMMENTS = 5;
 const ExitCode = {
   ERROR: 1,
   SUCCESS: 0,
@@ -13,6 +14,7 @@ const ExitCode = {
 const FILE_SENTENCES_PATH = `./data/sentences.txt`;
 const FILE_TITLES_PATH = `./data/titles.txt`;
 const FILE_CATEGORIES_PATH = `./data/categories.txt`;
+const FILE_COMMENTS_PATH = `./data/comments.txt`;
 const DEFAULT_PORT = 3000;
 const DEFAULT_EXPRESS_PORT = 8080;
 const HttpCode = {
@@ -23,6 +25,7 @@ const HttpCode = {
   UNAUTHORIZED: 401,
 };
 const PUBLIC_DIR = `public`;
+const MAX_ID_LENGTH = 6;
 
 
 module.exports = {
@@ -36,8 +39,11 @@ module.exports = {
   FILE_SENTENCES_PATH,
   FILE_TITLES_PATH,
   FILE_CATEGORIES_PATH,
+  FILE_COMMENTS_PATH,
   DEFAULT_PORT,
   HttpCode,
   DEFAULT_EXPRESS_PORT,
   PUBLIC_DIR,
+  MAX_ID_LENGTH,
+  MAX_COMMENTS,
 };
--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -2,7 +2,7 @@
 
 const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
-
+const {nanoid} = require(`nanoid`);
 
 const {
   shuffle,
@@ -18,6 +18,9 @@ const {
   FILE_SENTENCES_PATH,
   FILE_TITLES_PATH,
   FILE_CATEGORIES_PATH,
+  FILE_COMMENTS_PATH,
+  MAX_ID_LENGTH,
+  MAX_COMMENTS,
 } = require(`../../constants`);
 
 
@@ -28,8 +31,17 @@ const createDatePublication = {
   maxDate: new Date(currentDate.setMonth(currentDate.getMonth() - 3))
 };
 
-const generatePublications = (count, titles, categories, sentences) => (
+const generateComments = (count, comments) => (
+  Array(count).fill({}).map(()=> ({
+    id: nanoid(MAX_ID_LENGTH),
+    text: shuffle(comments).slice(0, getRandomInt(1, MAX_COMMENTS - 1)).join(``)
+  }))
+);
+
+const generatePublications = (count, titles, categories, sentences, comments) => (
   Array(count).fill({}).map(() => ({
+    id: nanoid(MAX_ID_LENGTH),
+    comments: generateComments(getRandomInt(1, MAX_COMMENTS), comments),
     title: titles[getRandomInt(0, titles.length - 1)],
     announce: shuffle(sentences).slice(0, MAX_ANNOUNCE_COUNT).join(``),
     fullText: shuffle(sentences).slice(0, getRandomInt(1, sentences.length - 1)).join(``),
@@ -55,10 +67,11 @@ module.exports = {
     const sentences = await readContent(FILE_SENTENCES_PATH);
     const titles = await readContent(FILE_TITLES_PATH);
     const categories = await readContent(FILE_CATEGORIES_PATH);
+    const comments = await readContent(FILE_COMMENTS_PATH);
 
     const [count] = args;
     const countPublications = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generatePublications(countPublications, titles, categories, sentences));
+    const content = JSON.stringify(generatePublications(countPublications, titles, categories, sentences, comments));
 
     if (countPublications > MAX_PUBLICATION_COUNT) {
       console.info(chalk.red(`Не больше 1000 объявлений.`));
